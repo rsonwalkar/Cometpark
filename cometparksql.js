dc3fc5a9e12900a43248e47c7aece228ac6f5125
Rohit <rohit@ubuntu.ubuntu-domain>
2014-04-01 23:31:42 -0500

    Adding authentication piece to the POST, PUT and DELETE REST calls. Also, adding support for customized parking space search.

--- cometparksql.js
+++ cometparksql.js
@@ -39,23 +39,6 @@ cometpark.get('/api', function (req, res) {
   	res.send('Success. Cometpark API is up and running :)');
 });
 
-// retreiving current parking status of the cometpark system
-// Example: curl http://localhost:4242/api/parkingstatus -X GET
-cometpark.get('/api/parkingstatus', function (req, res) {
-  connection.query('SELECT * FROM parkinglot', function(err, success) {
-	if(!err) {
-		res.statusCode = 200; 
-		return res.json(success);
-		console.log('Heres the data from parkinglot: ', success);
-	} else {
-		res.statusCode = 404;
-		res.type('text/plain');
-		res.send('Could not oull out parking lot data for you :(');
-		console.log('Could not select data from parking lot table and heres why: ', err);
-	}
-  });
-});
-
 // retreive current parking status of a specific parking spot
 // Example: curl http://localhost:4242/api/parkingstatus/P02 -X GET
 cometpark.get('/api/parkingstatus/:id', function (req, res) {
@@ -73,81 +56,143 @@ cometpark.get('/api/parkingstatus/:id', function (req, res) {
   });
 });
 
+// retreive current parking status of parking spots based on the lot and permit selection
+cometpark.get('/api/parkingstatus/:lot/:permit', function (req, res) {
+  console.log('Get request looks like this: ', req);
+  connection.query("SELECT * FROM parkinglot where plot='" + req.params.lot + "' and " + "permit='" + req.params.permit + "' and pstatus='vacant'", function(err, success) {
+	if(!err) {
+		res.statusCode = 200;
+		return res.send(success);
+		console.log('Heres the data from parkinglot: ', success);
+	} else {
+		res.statusCode = 404;
+		console.log('Could not select data from parking lot table and heres why: ', err);
+		return res.send('Could not pull data for that parking spot :(');
+	}
+  });
+});
+
+// retreiving current parking status of the cometpark system
+// Example: curl http://localhost:4242/api/parkingstatus -X GET
+cometpark.get('/api/parkingstatus', function (req, res) {
+  connection.query('SELECT * FROM parkinglot', function(err, success) {
+	if(!err) {
+		res.statusCode = 200; 
+		return res.json(success);
+		console.log('Heres the data from parkinglot: ', success);
+	} else {
+		res.statusCode = 404;
+		res.type('text/plain');
+		res.send('Could not oull out parking lot data for you :(');
+		console.log('Could not select data from parking lot table and heres why: ', err);
+	}
+  });
+});
+
 // Receive a POST request from the controller and update the same in the database.
 // POST is for creating a new entry into the database
-// Example: curl --data "lot=A&id=A01&status=occupied&permit=green" http://localhost:4242/api/parkingstatus -X POST
+// Example: curl --data "uname=swapnil&password=swapnil&lot=A&id=A01&status=occupied&permit=green" http://localhost:4242/api/parkingstatus -X POST
 cometpark.post('/api/parkingstatus', function(req, res) {
 	console.log('POST: ', req.body);
-	if(!req.body.hasOwnProperty('lot') || !req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('status') || !req.body.hasOwnProperty('permit')) {
+	if(!req.body.hasOwnProperty('uname') || !req.body.hasOwnProperty('password') || !req.body.hasOwnProperty('lot') || !req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('status') || !req.body.hasOwnProperty('permit')) {
 		res.statusCode = 400;
 		res.type('text/plain');
 		return res.send('Error 400 : Cometpark api POST syntax incorrect');
 	} else {
-		connection.query('INSERT into parkinglot values ("'+ req.body.lot + '","' + req.body.id + '","' + req.body.status + '","' + req.body.permit + '")', function(err, success) {
-			if(!err) {
-				res.statusCode = 200;
-				console.log('Entry successfully inserted into the parking lot table');
-				return res.send('Entry successfully inserted into the system :)');
-			} else {
-				res.statusCode = 404;
-				res.send('Could not insert an entry into the system :(');
-				console.log('Could not insert an entry into the parking lot table and heres why: ', err);
-			}
-  		});
-	}
-});
-
-// Updating a parking lot status using a PUT request
-// Example: curl --data "id=A03&status=occupied" http://localhost:4242/api/parkingstatus -X PUT
-cometpark.put('/api/parkingstatus', function(req, res) {
-	if(!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('status')) {
-		res.statusCode = 400;
-		res.type('text/plain');
-		return res.send('Error 400 : Cometpark api PUT syntax incorrect');
-	} else {
-		connection.query('SELECT * from parkinglot where pid="' + req.body.id + '"', function(err, success) {
-			if(!err) {
-				console.log('Such an entry exists in the parking lot table. Now updating as per your request');
-				connection.query('UPDATE parkinglot set pstatus="' + req.body.status + '" where pid="' + req.body.id + '"', function(err, success) {
+		connection.query('SELECT * from auth where uname="' + req.body.uname + '" and password="' + req.body.password + '"', function(err, success) {
+			if(!err && Object.keys(success).length != 0) {
+				console.log('Here is what we got from the auth table: ', success);
+				connection.query('INSERT into parkinglot values ("'+ req.body.lot + '","' + req.body.id + '","' + req.body.status + '","' + req.body.permit + '")', function(err, success) {
 					if(!err) {
-						res.statusCode = 200;
-						console.log('Parking lot status updated');
-						res.send('Parking lot status has been updated into the system as per your request :)');
+					res.statusCode = 200;
+					console.log('Entry successfully inserted into the parking lot table');
+					return res.send('Entry successfully inserted into the system :)');
 					} else {
 						res.statusCode = 404;
-						res.send('Could not update the parking spot status.' + 
-						'Check your REST call for all the required information.');
-						console.log('Could not update the parking spot status and here is why: ', err);
+						res.send('Could not insert an entry into the system :(');
+						console.log('Could not insert an entry into the parking lot table and heres why: ', err);
 					}
-				});
+  				});
 			} else {
 				res.statusCode = 404;
 				res.send('No such entry exists in the system so cannot update');
 				console.log('No such entry exists in the parking lot database so cannot update');
 			}
-  		});
+		});
 	}
-}); 
+});
+
+// Updating a parking lot status using a PUT request
+// Example: curl --data "uname=swapnil&password=swapnil&id=A03&status=occupied" http://localhost:4242/api/parkingstatus -X PUT
+cometpark.put('/api/parkingstatus', function(req, res) {
+	if(!req.body.hasOwnProperty('uname') || !req.body.hasOwnProperty('password') || !req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('status')) {
+		res.statusCode = 400;
+		res.type('text/plain');
+		return res.send('Error 400 : Cometpark api PUT syntax incorrect');
+	} else {
+		connection.query('SELECT * from auth where uname="' + req.body.uname + '" and password="' + req.body.password + '"', function(err, success) {
+			if(!err && Object.keys(success).length != 0) {
+				console.log('Here is what we got from the auth table: ', success);
+				connection.query('SELECT * from parkinglot where pid="' + req.body.id + '"', function(err, success) {
+					if(!err) {
+						console.log('Such an entry exists in the parking lot table. Now updating as per your request');
+						connection.query('UPDATE parkinglot set pstatus="' + req.body.status + '" where pid="' + req.body.id + '"', function(err, success) {
+							if(!err) {
+								res.statusCode = 200;
+								console.log('Parking lot status updated');
+								res.send('Parking lot status has been updated into the system as per your request :)');
+							} else {
+								res.statusCode = 404;
+								res.send('Could not update the parking spot status.' + 
+								'Check your REST call for all the required information.');
+								console.log('Could not update the parking spot status and here is why: ', err);
+							}
+						});
+					} else {
+						res.statusCode = 404;
+						res.send('No such entry exists in the system so cannot update');
+						console.log('No such entry exists in the parking lot database so cannot update');
+					}
+  				});
+			} else {
+				console.log('There was problem authorizing you as a user and here is why: ', err);
+				res.statusCode = 404;
+				res.send('There was a problem authorizing you as a user into the system :(');
+			}
+		}); 
+	}
+});
 
 // Deleting a parking lot. DELETE request. Note, this is only to be used by the admin
-// Example: curl --data "id=A01" http://localhost:4242/api/parkingstatus -X DELETE
-cometpark.delete('/api/parkingstatus', function(req, res) {
-	if(!req.body.hasOwnProperty('id')) {
+// Example: curl --data "uname=swapnil&password=swapnil&id=A01" http://localhost:4242/api/parkingstatus -X DELETE
+cometpark.del('/api/parkingstatus', function(req, res) {
+	if(!req.body.hasOwnProperty('uname') || !req.body.hasOwnProperty('password') || !req.body.hasOwnProperty('id')) {
 		res.statusCode = 400;
 		res.type('text/plain');
 		return res.send('Error 400 : Cometpark api DELETE syntax incorrect');
 	} else {
-		connection.query('DELETE from parkinglot where pid="', req.body.id, '"', function(err, success) {
-			if(!err) {
-				console.log('Parking spot deleted');
-				res.statusCode = 200;
-				res.send('Parking spot deleted as per your request :)');
+		//console.log('ID: ', req.body.uname, ' password: ', req.body.password);
+		connection.query('SELECT * from auth where uname="' + req.body.uname + '" and password="' + req.body.password + '"', function(err, success) {
+			if(!err && Object.keys(success).length != 0) {
+				console.log('Here is what we got from the auth table: ', success);
+				connection.query('DELETE from parkinglot where pid="' + req.body.id + '"', function(err, success) {
+					if(!err) {
+						console.log('Parking spot deleted');
+						res.statusCode = 200;
+						res.send('Parking spot deleted as per your request :)');
+					} else {
+						console.log('Could not delete the parking spot and here is why: ', err);
+						res.statusCode = 404;
+						res.send('Could not delete the parking spot from the system :(');
+					}
+				});
 			} else {
-				console.log('Could not delete the parking spot and here is why: ', err);
+				console.log('There was problem authorizing you as a user and here is why: ', err);
 				res.statusCode = 404;
-				res.send('Could not delete the parking spot from the system :(');
+				res.send('There was a problem authorizing you as a user into the system :(');
 			}
 		});
+
 	}
 }); 
 
